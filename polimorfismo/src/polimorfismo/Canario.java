2c91e11676bb2263ebdf64aa413e36b80a70e07a
luisbohorquez18 <62857482+luisbohorquez18@users.noreply.github.com>
2020-03-29 21:42:03 -0500

    Add files via upload

---
+++ polimorfismo/src/polimorfismo/Canario.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package polimorfismo;
+
+/**
+ *
+ * @author Dell
+ */
+public class Canario implements PuedeCantar { 
+    		public void cantar() { 
+      		  System.out.println("pio pio pio"); 
+   	 } 
+}
+
+
